72006d19f7f10917e9502b0d43f321e94eb0751d
Giraphi <hoeps.raphael@gmail.com>
2023-07-27 23:48:25 +0200

    get rid of swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 // import reactRefresh from "@vitejs/plugin-react-refresh";
 import svgr from "vite-plugin-svgr";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
