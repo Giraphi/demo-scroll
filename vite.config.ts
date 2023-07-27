304d0b49b4ed02e5435157c2d19e442def81c8d9
Giraphi <hoeps.raphael@gmail.com>
2023-07-27 23:08:24 +0200

    change setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+// import reactRefresh from "@vitejs/plugin-react-refresh";
 import svgr from "vite-plugin-svgr";
-// import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh(), svgr()],
+    plugins: [react(), svgr()],
 });
